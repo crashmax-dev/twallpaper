af1c2ac5b13f738a1010adb14064ab0775dad4b5
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-03-07 03:10:19 +0800

    refactor

--- src/telegram-wallpaper.ts
+++ src/telegram-wallpaper.ts
@@ -15,43 +15,48 @@ interface Options {
   scrollAnimate?: boolean
 }
 
+const curve: number[] = [
+  0, 0.25, 0.50, 0.75, 1, 1.5, 2, 2.5, 3, 3.5, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+  13, 14, 15, 16, 17, 18, 18.3, 18.6, 18.9, 19.2, 19.5, 19.8, 20.1, 20.4, 20.7,
+  21.0, 21.3, 21.6, 21.9, 22.2, 22.5, 22.8, 23.1, 23.4, 23.7, 24.0, 24.3, 24.6,
+  24.9, 25.2, 25.5, 25.8, 26.1, 26.3, 26.4, 26.5, 26.6, 26.7, 26.8, 26.9, 27
+]
+
+const positions: Positions[] = [
+  { x: 0.80, y: 0.10 },
+  { x: 0.60, y: 0.20 },
+  { x: 0.35, y: 0.25 },
+  { x: 0.25, y: 0.60 },
+  { x: 0.20, y: 0.90 },
+  { x: 0.40, y: 0.80 },
+  { x: 0.65, y: 0.75 },
+  { x: 0.75, y: 0.40 }
+]
+
 export class TelegramWallpaper {
-  private _width = 50
-  private _height = 50
-  private _phase = 0
-  private _tail = 0
-  private _tails = 90
-  private _scrolltails = 50
-  private _ts = 0
-  private _fps = 15
-  private _frametime = 1000 / this._fps
-  private _scrollDelta = 0
-  private _scrollTicking = false
-  private _raf: ReturnType<typeof requestAnimationFrame> | null = null
-  private _interval: ReturnType<typeof setInterval> | null = null
-  private _hc: HTMLCanvasElement
-  private _hctx: CanvasRenderingContext2D
-  private _canvas: HTMLCanvasElement
-  private _ctx: CanvasRenderingContext2D
-  private _frames: ImageData[] = []
-  private _colors: RgbColor[] = []
-  private _curve = [
-    0, 0.25, 0.50, 0.75, 1, 1.5, 2, 2.5, 3, 3.5, 4, 5, 6, 7, 8, 9, 10, 11, 12,
-    13, 14, 15, 16, 17, 18, 18.3, 18.6, 18.9, 19.2, 19.5, 19.8, 20.1, 20.4, 20.7,
-    21.0, 21.3, 21.6, 21.9, 22.2, 22.5, 22.8, 23.1, 23.4, 23.7, 24.0, 24.3, 24.6,
-    24.9, 25.2, 25.5, 25.8, 26.1, 26.3, 26.4, 26.5, 26.6, 26.7, 26.8, 26.9, 27
-  ]
-  private _positions = [
-    { x: 0.80, y: 0.10 },
-    { x: 0.60, y: 0.20 },
-    { x: 0.35, y: 0.25 },
-    { x: 0.25, y: 0.60 },
-    { x: 0.20, y: 0.90 },
-    { x: 0.40, y: 0.80 },
-    { x: 0.65, y: 0.75 },
-    { x: 0.75, y: 0.40 }
-  ]
-  private _phases = this._positions.length
+  private width = 50
+  private height = 50
+  private phase = 0
+  private tail = 0
+  private tails = 90
+  private scrolltails = 50
+  private timestamp = 100
+  private fps = 15
+  private frametime = 1000 / this.fps
+  private scrollDelta = 0
+  private scrollTicking = false
+  private frames: ImageData[] = []
+  private colors: RgbColor[] = []
+  private curve = curve
+  private positions = positions
+  private phases = positions.length
+  private interval: ReturnType<typeof setInterval> | null = null
+  private raf: ReturnType<typeof requestAnimationFrame> | null = null
+
+  private hc: HTMLCanvasElement
+  private hctx: CanvasRenderingContext2D
+  private canvas: HTMLCanvasElement
+  private ctx: CanvasRenderingContext2D
 
   constructor({
     canvas,
@@ -82,7 +87,7 @@ export class TelegramWallpaper {
   }
 
   private getPositions(shift: number): Positions[] {
-    const positions = [...this._positions]
+    const positions = [...this.positions]
     while (shift > 0) {
       positions.push(positions.shift()!)
       shift--
@@ -97,19 +102,19 @@ export class TelegramWallpaper {
   }
 
   private curPosition(phase: number, tail: number): Positions[] {
-    tail %= this._tails
-    const pos = this.getPositions(phase % this._phases)
+    tail %= this.tails
+    const pos = this.getPositions(phase % this.phases)
 
     if (tail) {
-      const next_pos = this.getPositions(++phase % this._phases)
-      const d1x = (next_pos[0].x - pos[0].x) / this._tails
-      const d1y = (next_pos[0].y - pos[0].y) / this._tails
-      const d2x = (next_pos[1].x - pos[1].x) / this._tails
-      const d2y = (next_pos[1].y - pos[1].y) / this._tails
-      const d3x = (next_pos[2].x - pos[2].x) / this._tails
-      const d3y = (next_pos[2].y - pos[2].y) / this._tails
-      const d4x = (next_pos[3].x - pos[3].x) / this._tails
-      const d4y = (next_pos[3].y - pos[3].y) / this._tails
+      const next_pos = this.getPositions(++phase % this.phases)
+      const d1x = (next_pos[0].x - pos[0].x) / this.tails
+      const d1y = (next_pos[0].y - pos[0].y) / this.tails
+      const d2x = (next_pos[1].x - pos[1].x) / this.tails
+      const d2y = (next_pos[1].y - pos[1].y) / this.tails
+      const d3x = (next_pos[2].x - pos[2].x) / this.tails
+      const d3y = (next_pos[2].y - pos[2].y) / this.tails
+      const d4x = (next_pos[3].x - pos[3].x) / this.tails
+      const d4y = (next_pos[3].y - pos[3].y) / this.tails
 
       return [
         {
@@ -135,39 +140,39 @@ export class TelegramWallpaper {
   }
 
   private changeTail(diff: number): void {
-    this._tail += diff
+    this.tail += diff
 
-    while (this._tail >= this._tails) {
-      this._tail -= this._tails
-      this._phase++
+    while (this.tail >= this.tails) {
+      this.tail -= this.tails
+      this.phase++
 
-      if (this._phase >= this._phases) {
-        this._phase -= this._phases
+      if (this.phase >= this.phases) {
+        this.phase -= this.phases
       }
     }
 
-    while (this._tail < 0) {
-      this._tail += this._tails
-      this._phase--
+    while (this.tail < 0) {
+      this.tail += this.tails
+      this.phase--
 
-      if (this._phase < 0) {
-        this._phase += this._phases
+      if (this.phase < 0) {
+        this.phase += this.phases
       }
     }
   }
 
   private onWheel(event: WheelEvent): void {
-    this._scrollDelta += event.deltaY
+    this.scrollDelta += event.deltaY
 
-    if (!this._scrollTicking) {
+    if (!this.scrollTicking) {
       requestAnimationFrame(() => this.drawOnWheel())
-      this._scrollTicking = true
+      this.scrollTicking = true
     }
   }
 
   private drawOnWheel(): void {
-    let diff = this._scrollDelta / this._scrolltails
-    this._scrollDelta %= this._scrolltails
+    let diff = this.scrollDelta / this.scrolltails
+    this.scrollDelta %= this.scrolltails
 
     diff = diff > 0 ?
       Math.floor(diff) :
@@ -175,34 +180,34 @@ export class TelegramWallpaper {
 
     if (diff) {
       this.changeTail(diff)
-      const curPos = this.curPosition(this._phase, this._tail)
+      const curPos = this.curPosition(this.phase, this.tail)
       this.drawGradient(curPos)
     }
 
-    this._scrollTicking = false
+    this.scrollTicking = false
   }
 
   private drawNextPositionAnimated(): void {
-    if (this._frames.length > 0) {
-      const id = this._frames.shift()!
+    if (this.frames.length > 0) {
+      const id = this.frames.shift()!
       this.drawImageData(id)
-    } else if (this._interval) {
-      clearInterval(this._interval)
+    } else if (this.interval) {
+      clearInterval(this.interval)
     }
   }
 
   private getGradientImageData(positions: Positions[]): ImageData {
-    const id = this._hctx.createImageData(this._width, this._height)
+    const id = this.hctx.createImageData(this.width, this.height)
     const pixels = id.data
     let offset = 0
 
-    for (let y = 0; y < this._height; y++) {
-      const directPixelY = y / this._height
+    for (let y = 0; y < this.height; y++) {
+      const directPixelY = y / this.height
       const centerDistanceY = directPixelY - 0.5
       const centerDistanceY2 = centerDistanceY * centerDistanceY
 
-      for (let x = 0; x < this._width; x++) {
-        const directPixelX = x / this._width
+      for (let x = 0; x < this.width; x++) {
+        const directPixelX = x / this.width
 
         const centerDistanceX = directPixelX - 0.5
         const centerDistance = Math.sqrt(centerDistanceX * centerDistanceX + centerDistanceY2)
@@ -221,7 +226,7 @@ export class TelegramWallpaper {
         let g = 0.0
         let b = 0.0
 
-        for (let i = 0; i < this._colors.length; i++) {
+        for (let i = 0; i < this.colors.length; i++) {
           const colorX = positions[i].x
           const colorY = positions[i].y
 
@@ -232,9 +237,9 @@ export class TelegramWallpaper {
           distance = distance * distance * distance * distance
           distanceSum += distance
 
-          r += distance * this._colors[i].r / 255
-          g += distance * this._colors[i].g / 255
-          b += distance * this._colors[i].b / 255
+          r += distance * this.colors[i].r / 255
+          g += distance * this.colors[i].g / 255
+          b += distance * this.colors[i].b / 255
         }
 
         pixels[offset++] = r / distanceSum * 255.0
@@ -248,8 +253,8 @@ export class TelegramWallpaper {
   }
 
   private drawImageData(id: ImageData): void {
-    this._hctx.putImageData(id, 0, 0)
-    this._ctx.drawImage(this._hc, 0, 0, 50, 50)
+    this.hctx.putImageData(id, 0, 0)
+    this.ctx.drawImage(this.hc, 0, 0, 50, 50)
   }
 
   private drawGradient(pos: Positions[]): void {
@@ -259,18 +264,18 @@ export class TelegramWallpaper {
   private doAnimate(): void {
     const now = +Date.now()
 
-    if (!document.hasFocus() || now - this._ts < this._frametime) {
-      this._raf = requestAnimationFrame(() => this.doAnimate())
+    if (!document.hasFocus() || now - this.timestamp < this.frametime) {
+      this.raf = requestAnimationFrame(() => this.doAnimate())
       return
     }
 
-    this._ts = now
+    this.timestamp = now
     this.changeTail(1)
 
-    const cur_pos = this.curPosition(this._phase, this._tail)
+    const cur_pos = this.curPosition(this.phase, this.tail)
     this.drawGradient(cur_pos)
 
-    this._raf = requestAnimationFrame(() => this.doAnimate())
+    this.raf = requestAnimationFrame(() => this.doAnimate())
   }
 
   private init(canvas: HTMLCanvasElement): void {
@@ -284,38 +289,38 @@ export class TelegramWallpaper {
       const color = this.hexToRgb(colors[i])
 
       if (color) {
-        this._colors.push(color)
+        this.colors.push(color)
       }
     }
 
-    if (!this._hc) {
-      this._hc = document.createElement('canvas')
-      this._hc.width = this._width
-      this._hc.height = this._height
-      this._hctx = this._hc.getContext('2d')!
+    if (!this.hc) {
+      this.hc = document.createElement('canvas')
+      this.hc.width = this.width
+      this.hc.height = this.height
+      this.hctx = this.hc.getContext('2d')!
     }
 
-    this._canvas = canvas
-    this._ctx = this._canvas.getContext('2d')!
+    this.canvas = canvas
+    this.ctx = this.canvas.getContext('2d')!
     this.update()
   }
 
   update(): void {
-    const pos = this.curPosition(this._phase, this._tail)
+    const pos = this.curPosition(this.phase, this.tail)
     this.drawGradient(pos)
   }
 
   toNextPosition(): void {
-    if (!this._interval) {
+    if (!this.interval) {
       return
     }
 
-    clearInterval(this._interval)
-    this._frames = []
+    clearInterval(this.interval)
+    this.frames = []
 
-    const prev_pos = this.getPositions(this._phase % this._phases)
-    this._phase++
-    const pos = this.getPositions(this._phase % this._phases)
+    const prev_pos = this.getPositions(this.phase % this.phases)
+    this.phase++
+    const pos = this.getPositions(this.phase % this.phases)
 
     const h = 27
     const d1x = (pos[0].x - prev_pos[0].x) / h
@@ -330,34 +335,34 @@ export class TelegramWallpaper {
     for (let frame = 0; frame < 60; frame++) {
       const cur_pos: Positions[] = [
         {
-          x: prev_pos[0].x + d1x * this._curve[frame],
-          y: prev_pos[0].y + d1y * this._curve[frame]
+          x: prev_pos[0].x + d1x * this.curve[frame],
+          y: prev_pos[0].y + d1y * this.curve[frame]
         },
         {
-          x: prev_pos[1].x + d2x * this._curve[frame],
-          y: prev_pos[1].y + d2y * this._curve[frame]
+          x: prev_pos[1].x + d2x * this.curve[frame],
+          y: prev_pos[1].y + d2y * this.curve[frame]
         },
         {
-          x: prev_pos[2].x + d3x * this._curve[frame],
-          y: prev_pos[2].y + d3y * this._curve[frame]
+          x: prev_pos[2].x + d3x * this.curve[frame],
+          y: prev_pos[2].y + d3y * this.curve[frame]
         },
         {
-          x: prev_pos[3].x + d4x * this._curve[frame],
-          y: prev_pos[3].y + d4y * this._curve[frame]
+          x: prev_pos[3].x + d4x * this.curve[frame],
+          y: prev_pos[3].y + d4y * this.curve[frame]
         }
       ]
 
-      this._frames.push(this.getGradientImageData(cur_pos))
+      this.frames.push(this.getGradientImageData(cur_pos))
     }
 
-    this._interval = setInterval(() => {
+    this.interval = setInterval(() => {
       this.drawNextPositionAnimated()
     }, 1000 / 30)
   }
 
   animate(start: boolean): void {
-    if (!start && this._raf) {
-      return cancelAnimationFrame(this._raf)
+    if (!start && this.raf) {
+      return cancelAnimationFrame(this.raf)
     }
 
     this.doAnimate()
