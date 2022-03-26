209f1de5584b3334a807c32a834514e834cf6e62
crashmax-dev <crashmax.dev@gmail.com>
2022-03-26 12:33:08 +0000

    Deploying to gh-pages from @ crashmax-dev/twallpaper@7e0edd8c714200fb7afc82e127f736e29048e63b 🚀

---
+++ twallpaper.d.ts
@@ -0,0 +1,67 @@
+declare type Container = HTMLElement | Element | null;
+export interface PatternOptions {
+    image?: string;
+    mask?: boolean;
+    background?: string;
+    blur?: number;
+    size?: string;
+    opacity?: number;
+}
+export interface TWallpaperOptions {
+    fps?: number;
+    tails?: number;
+    colors: string[];
+    animate?: boolean;
+    scrollAnimate?: boolean;
+    pattern?: PatternOptions;
+}
+export declare class TWallpaper {
+    private width;
+    private height;
+    private phase;
+    private tail;
+    private tails;
+    private scrollTails;
+    private timestamp;
+    private frametime;
+    private scrollDelta;
+    private scrollTicking;
+    private frames;
+    private rgb;
+    private curve;
+    private positions;
+    private phases;
+    private interval;
+    private raf;
+    private container;
+    private hc;
+    private hctx;
+    private canvas;
+    private ctx;
+    private pattern;
+    constructor(container: Container, options: TWallpaperOptions);
+    private hexToRgb;
+    private getPositions;
+    private curPosition;
+    private changeTail;
+    private onWheel;
+    private drawOnWheel;
+    private drawNextPositionAnimated;
+    private getGradientImageData;
+    private drawImageData;
+    private drawGradient;
+    private requestAnimate;
+    private doAnimate;
+    init({ fps, tails, colors, pattern, animate, container, scrollAnimate }: TWallpaperOptions & {
+        container?: Container;
+    }): void;
+    dispose(): void;
+    updateTails(tails?: number): void;
+    updateFrametime(fps?: number): void;
+    updatePattern(pattern: PatternOptions): void;
+    updateColors(colors: string[]): void;
+    toNextPosition(callback?: () => void): void;
+    animate(start?: boolean): void;
+    scrollAnimate(start?: boolean): void;
+}
+export {};
